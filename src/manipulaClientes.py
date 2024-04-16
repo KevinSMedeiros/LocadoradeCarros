951d55117577ed09928ae021b8d0a0d56aaba134
Pedro G. Vargas <127370660+pedrovargascba@users.noreply.github.com>
2024-04-15 23:35:36 -0400

    adicionando funcionalidade de localizar locações do cliente

--- src/manipulaClientes.py
+++ src/manipulaClientes.py
@@ -1,4 +1,6 @@
 import manipulaCSV as mcsv
+import manipulaCarros as mcar
+import manipulaLocacoes as mloc
 import apresentacao
 
 def carregar() ->list: 
@@ -83,3 +85,73 @@ def busca1Cliente(listaClientes : list, cpf : str) -> dict:
         if cliente['CPF'] == cpf:
             return cliente
     return None
+
+def busca1ClientePorNome(listaClientes : list, nome : str) -> dict:
+    '''
+    Busca um cliente na lista de clientes
+    Parâmetros
+    ----------
+    listaClientes: Lista atual dos clientes
+    nome: nome do cliente a ser buscado
+    Retorno
+    -------
+    Retorna um dicionário com os dados do cliente
+    '''
+    for cliente in listaClientes:
+        if cliente['Nome'] == nome:
+            return cliente
+    return None
+
+def locacoesDoCliente() -> bool:
+    '''
+        Varre todas as locações em busca daquelas que ainda estão em andamento
+        Quando encontra alguma, o algoritmo puxa todas as informações relacionadas dos outros arquivos
+
+        No final, o valor total é calculado com base na data atual e as informações são printadas
+    '''
+
+    print("### LOCAÇÕES FINALIZADAS DO CLIENTE ###\n")
+
+    listaCarros = mcar.carregar()
+    listaLocacoes = mloc.carregar()
+    listaClientes = carregar()
+
+    escolha = int(input("Você quer pesquisar cliente por nome ou CPF?\n1.CPF\n2.Nome\n"))
+    if escolha == 1:
+        CPF = input("Digite o CPF do cliente: ")
+    else:
+        Nome = input("Digite o nome do cliente: ")
+
+    for i in range(len(listaLocacoes)):
+        locacao = mcsv.retornaNaLinha("Locacao.csv", i)  # puxa as informações da locação com o id digitado
+
+        if escolha == 1 and CPF == locacao["CPF"]:
+            valido = True
+        else:
+            if escolha == 2 and busca1ClientePorNome(listaClientes, Nome)['CPF'] == locacao["CPF"]:
+                valido = True
+            else:
+                valido = False
+
+        if valido and locacao["DataDevolucao"] != "00/00/0000 00:00":
+
+            carro = mcar.buscaCarroPorId(listaCarros, locacao["IdCarro"])
+
+            if escolha == 1:
+                cliente = busca1Cliente(listaClientes, CPF)
+            else:
+                cliente = busca1ClientePorNome(listaClientes, Nome)
+
+            if cliente is None:
+                print("Erro ao carregar o cliente")
+
+            distanciaPercorrida = int(locacao["KmFinal"]) - int(locacao["KmInicial"])
+
+            print(f"\nID DE LOCAÇÃO: {locacao["IdLocacao"]}")
+            print(f"Placa do carro: {carro["Placa"]}")
+            print(f"Início da Locação: {locacao["DataInicio"]}")
+            print(f"Final da Locação: {locacao["DataDevolucao"]}")
+            print(f"Quilometragem percorrida: {distanciaPercorrida}")
+            print(f"Valor total: {locacao["ValorTotal"]}")
+
+    return True
